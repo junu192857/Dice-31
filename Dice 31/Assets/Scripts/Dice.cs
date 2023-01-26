12f3ec10a45265e765470bdaa9ca1fb6694df2da
joongwon <winterdawnlight@snu.ac.kr>
2023-01-26 15:43:54 +0900

    add missing colors

--- Dice 31/Assets/Scripts/Dice.cs
+++ Dice 31/Assets/Scripts/Dice.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Color {
+    Yellow,
+    Green,
+    Red,
+    Purple
+}
+
 public abstract class Dice : MonoBehaviour
 {
     public abstract IEnumerator Roll();
